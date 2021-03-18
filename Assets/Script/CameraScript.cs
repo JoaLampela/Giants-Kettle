44d8e5d536b2e9787bf74ccb4cadc58ce4298176
GGeru <56148582+GGeru@users.noreply.github.com>
2021-03-18 21:47:00 +0200

    Fixed basic player movement
    Took the player out of the UI because it was not a good idea. Made a player prefab and tuned the player and camera scripts so that they are easier to move between scenes.

--- Assets/Script/CameraScript.cs
+++ Assets/Script/CameraScript.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform playerTransform;
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 5f;
+    private Transform playerTransform;
     // Start is called before the first frame update
     void Start()
     {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        //set the camera to the players position
+        transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, transform.position.z);
         
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
     {
         Vector3 position = transform.position;
-
+        
         position.x = playerTransform.position.x;
         position.y = playerTransform.position.y;
+        //smoothly transforms from the cameras position to the players position
+        position = Vector3.Lerp(transform.position, position, smoothSpeed * Time.fixedDeltaTime);
 
         //sets the player position (x & y) as the position of the camera
         transform.position = position;
-
     }
 }
--- Assets/Script/MovementScript.cs
+++ Assets/Script/MovementScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public float speed;
+    public float speed = 10;
     private Vector2 movement;
     Rigidbody2D playerRB;
     // Start is called before the first frame update
@@ -17,15 +17,22 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetMovementInput();
+    }
+
+    private void FixedUpdate()
+    {
+        //move the player
+        playerRB.velocity = new Vector2(movement.x * speed, movement.y * speed);
+    }
+
+    //get input for the player movement
+    void GetMovementInput()
+    {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         //.normalized caps the vector length to 1, so that diagonal movement works properly
         movement = new Vector2(moveX, moveY).normalized;
-    }
-
-    private void FixedUpdate()
-    {
-        playerRB.velocity = new Vector2(movement.x * speed, movement.y * speed);
     }
 }
