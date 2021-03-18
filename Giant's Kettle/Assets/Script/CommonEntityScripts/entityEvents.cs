e6968ee9083b1fba68e9ec920c4519a82ba41630
kaarlagd <max.gun.kaarla@gmail.com>
2021-03-18 10:12:00 +0200

    Added rough plan for entity event system
    Added 2 script folders and added plan for entity event system

---
+++ Giant's Kettle/Assets/Script/CommonEntityScripts/entityEvents.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class entityEvents : MonoBehaviour
+{
+    //OnHitGameObject
+    //OnHitEnemyGameObject
+    //OnKillEnemy
+    //OnLoseSpirit
+    //OnLoseEnergy
+    //OnGainRage
+    //OnLoseHealth
+    //OnGainHealth
+    //OnGetCCd
+    //OnCastAbility
+    //OnNormalAttack
+}
