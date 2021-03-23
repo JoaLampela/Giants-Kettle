d29b7b9687166540c1942f56adb1c3df73b4c709
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-03-23 14:33:30 +0200

    Main character and basic (temp) animations added

--- Assets/Script/MovementScript.cs
+++ Assets/Script/MovementScript.cs
@@ -7,10 +7,12 @@ public class MovementScript : MonoBehaviour
     public float speed = 10;
     private Vector2 movement;
     Rigidbody2D playerRB;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
     }
 
@@ -23,16 +25,29 @@ public class MovementScript : MonoBehaviour
     private void FixedUpdate()
     {
         //move the player
+
         playerRB.velocity = new Vector2(movement.x * speed, movement.y * speed);
     }
 
     //get input for the player movement
     void GetMovementInput()
     {
+
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
         //.normalized caps the vector length to 1, so that diagonal movement works properly
+        if (moveX == 1 || moveX == -1 || moveY == 1 || moveY == -1)
+        {
+            animator.SetBool("Walking", true);
+            if (moveX == 1 || moveX == -1)
+                gameObject.transform.localScale = new Vector3(moveX, 1, 1);
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+
         movement = new Vector2(moveX, moveY).normalized;
     }
 }
