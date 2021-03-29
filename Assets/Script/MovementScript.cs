85148d720d876656fff176d5ddd7fb21e1fd6682
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-03-29 10:47:31 +0300

    Made changes to movement script
    - walk slower walking backwards
    - walk slower whilst attacking

--- Assets/Script/MovementScript.cs
+++ Assets/Script/MovementScript.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
-    public float speed = 10;
+    public float speed = 8;
+    float originalMovmentSpeed;
     public GameObject rightArm;
     public Camera playerCamera;
     private Vector2 movement;
@@ -12,7 +13,11 @@ public class MovementScript : MonoBehaviour
     private bool walkingBackWards;
     private bool attacking = false;
     private Animator animator;
+    private Vector2 lookDirection;
     public BoxCollider2D swordCollider;
+    public int lastHorizontalAxisRaw;
+
+
 
     //Animator animator;
     // Start is called before the first frame update
@@ -20,7 +25,8 @@ public class MovementScript : MonoBehaviour
     {
         playerRB = GetComponent<Rigidbody>();
         animator = GetComponent<Animator>();
-
+        lastHorizontalAxisRaw = 0;
+        originalMovmentSpeed = speed;
     }
 
     // Update is called once per frame
@@ -32,8 +38,16 @@ public class MovementScript : MonoBehaviour
     private void FixedUpdate()
     {
         //move the player
+        //slower if looking the wrong way
+        if (Vector2.Dot(lookDirection, playerRB.velocity) < 0)
+        {
+            playerRB.velocity = new Vector2(movement.x * speed * 0.7f, movement.y * speed * 0.7f);
+        }
+        else
+        {
+            playerRB.velocity = new Vector2(movement.x * speed, movement.y * speed);
+        }
 
-        playerRB.velocity = new Vector2(movement.x * speed, movement.y * speed);
 
     }
 
@@ -46,6 +60,7 @@ public class MovementScript : MonoBehaviour
             animator.SetTrigger("Player_Swing");
             //Debug.Log("Swing sword pog");
             attacking = true;
+            speed = speed / 1.5f;
             StartCoroutine(swingAnimationCoolDown(0.5f));
             swordCollider.enabled = true;
         }
@@ -54,10 +69,13 @@ public class MovementScript : MonoBehaviour
 
             Vector2 difference = playerCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, playerCamera.transform.position.z)) - rightArm.transform.position;
             difference.Normalize();
+            lookDirection = -difference;
             float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg - 90f;
             //Debug.Log(rotationZ);
             if (rotationZ < 0 && rotationZ > -180)
+            {
                 gameObject.transform.localScale = new Vector3(-1, 1, 1);
+            }
             else
             {
                 gameObject.transform.localScale = new Vector3(1, 1, 1);
@@ -68,16 +86,25 @@ public class MovementScript : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
+        if (moveY != lastHorizontalAxisRaw)
+        {
+            if (moveY == -1)
+            {
+
+            }
+            else if (moveY == 1)
+            {
+
+            }
+        }
 
         if (moveX == 1 || moveX == -1 || moveY == 1 || moveY == -1)
         {
             animator.SetBool("Walking", true);
-            if (moveX == 1)
+            if (moveY == 1)
                 walkingBackWards = false;
-            else if (moveX == -1)
+            else if (moveY == -1)
                 walkingBackWards = true;
-
-
         }
         else
         {
@@ -91,6 +118,7 @@ public class MovementScript : MonoBehaviour
     IEnumerator swingAnimationCoolDown(float CoolDown)
     {
         yield return new WaitForSeconds(CoolDown);
+        speed = originalMovmentSpeed;
         attacking = false;
         swordCollider.enabled = false;
     }
