e812447be49f46181bd51558a4cbb09d391d17fa
JarnoTiainen <tiainenjarno@gmail.com>
2021-04-18 05:55:02 +0300

    Ability bar

---
+++ Assets/Script/UI/RightAbility.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RightAbility : MonoBehaviour
+{
+    public Image icon;
+    public Image iconBG;
+    private float cooldown;
+    private bool isCooldown = false;
+    private Item _item = null;
+
+
+
+    private void Start()
+    {
+        SetIcon();
+    }
+
+    private void Update()
+    {
+        AbilityUpdate();
+    }
+
+
+
+    void AbilityUpdate()
+    {
+        if (_item != null)
+        {
+            if (isCooldown == false && Time.timeScale != 0)
+            {
+                isCooldown = true;
+                icon.fillAmount = 1;
+            }
+
+            if (isCooldown && Time.timeScale != 0)
+            {
+                icon.fillAmount -= 1 / cooldown * Time.deltaTime;
+
+                if (icon.fillAmount <= 0)
+                {
+                    icon.fillAmount = 0;
+                    isCooldown = false;
+                    cooldown = _item.currentCooldownAbility1;
+                }
+            }
+        }
+    }
+
+
+    void SetAbility(Item item)
+    {
+        _item = item;
+        cooldown = _item.currentCooldownAbility1;
+        SetIcon();
+    }
+
+    void RemoveAbility()
+    {
+        _item = null;
+        SetIcon();
+    }
+
+    void SetIcon()
+    {
+        if (_item == null)
+        {
+            icon.sprite = null;
+            iconBG.sprite = null;
+        }
+    }
+}
