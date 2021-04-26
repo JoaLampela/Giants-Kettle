1c15c50974e24b9fcbe8f821a457aba150444a68
HawkOfCinders <39711821+HawkOfCinders@users.noreply.github.com>
2021-04-27 02:49:27 +0300

    Shield fixes n stuff

--- Assets/Script/Player/Player_Animations.cs
+++ Assets/Script/Player/Player_Animations.cs
@@ -118,7 +118,7 @@ public class Player_Animations : MonoBehaviour, IEntityAnimations
     public void SwitchToOffHandShield(GameObject inGameObject)
     {
         UnequipLeftHandBools();
-        leftHandWeapon = Instantiate(inGameObject, leftHand.transform.position, new Quaternion(0, 0, 0, 0), rightHand.transform);
+        leftHandWeapon = Instantiate(inGameObject, leftHand.transform.position, new Quaternion(0, 0, 0, 0), leftHand.transform);
         offHandUsingShield = true;
         animator.SetBool("ShieldOffHandEquiped", true);
     }
