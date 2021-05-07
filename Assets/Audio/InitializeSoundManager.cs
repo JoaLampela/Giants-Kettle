2a4a12f10c0251201fff6f5771dce3b701743b5f
JarnoTiainen <tiainenjarno@gmail.com>
2021-05-07 20:52:52 +0300

    Revert "Revert "Button sounds etc.""
    This reverts commit b9cf3198e6ab719a9e70aff2413e89bbbc8e7be4.

---
+++ Assets/Audio/InitializeSoundManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeSoundManager : MonoBehaviour
+{
+    void Start()
+    {
+        SoundManager.Initialize();
+    }
+}
