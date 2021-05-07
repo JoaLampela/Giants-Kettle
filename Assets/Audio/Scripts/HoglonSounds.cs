e6e46e224a54c1d2f30e9a5d964ff44e6b13c5a0
JarnoTiainen <tiainenjarno@gmail.com>
2021-05-08 01:05:24 +0300

    Death sounds for enemies

---
+++ Assets/Audio/Scripts/HoglonSounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoglonSounds : MonoBehaviour
+{
+    private EntityEvents events;
+    private void Start()
+    {
+        events = gameObject.GetComponent<EntityEvents>();
+        Subscribe();
+    }
+    private void OnDisable()
+    {
+        Unsubscribe();
+    }
+    private void PlayDeathSound(GameObject spaghetti, GameObject bolognese)
+    {
+        SoundManager.PlaySound(SoundManager.Sound.BossDie);
+    }
+
+    private void Subscribe()
+    {
+        events.OnDie += PlayDeathSound;
+    }
+    private void Unsubscribe()
+    {
+        events.OnDie -= PlayDeathSound;
+    }
+}
---
+++ Assets/Audio/Scripts/SummonerSounds.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummonerSounds : MonoBehaviour
+{
+    private EntityEvents events;
+    private void Start()
+    {
+        events = gameObject.GetComponent<EntityEvents>();
+        Subscribe();
+    }
+    private void OnDisable()
+    {
+        Unsubscribe();
+    }
+    private void PlayDeathSound(GameObject spaghetti, GameObject bolognese)
+    {
+        SoundManager.PlaySound(SoundManager.Sound.SummonerDie);
+    }
+
+    private void Subscribe()
+    {
+        events.OnDie += PlayDeathSound;
+    }
+    private void Unsubscribe()
+    {
+        events.OnDie -= PlayDeathSound;
+    }
+}
