918869e29a0653b00d7d061688c8f0e6a12c50e6
kaarlagd <max.gun.kaarla@gmail.com>
2021-05-09 18:51:10 +0300

    difficulty fixed goblin orbs removed

--- Assets/Script/UI/MainMenu.cs
+++ Assets/Script/UI/MainMenu.cs
@@ -8,7 +8,6 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenuButtons;
     public GameObject selectLevel;
     public GameObject loadingScreen;
-    [SerializeField] GameDifficultyManagerScript gameDifficultyManagerScript;
 
 
     public void PlayLevel1()
@@ -23,31 +22,55 @@ public class MainMenu : MonoBehaviour
 
     public void Easy()
     {
+        StartCoroutine(Easy2());
+
+    }
+    IEnumerator Easy2()
+    {
         loadingScreen.SetActive(true);
-        gameDifficultyManagerScript.difficulty = GameDifficultyManagerScript.Difficulty.Easy;
-
+        GameObject.Find("GameDifficultyManager").GetComponent<GameDifficultyManagerScript>().SetDifficulty(GameDifficultyManagerScript.Difficulty.Easy);
+        Debug.Log("Setting Easy");
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-
     }
 
     public void Normal()
     {
+        StartCoroutine(Normal2());
+    }
+    IEnumerator Normal2()
+    {
         loadingScreen.SetActive(true);
-        gameDifficultyManagerScript.difficulty = GameDifficultyManagerScript.Difficulty.Normal;
+        GameObject.Find("GameDifficultyManager").GetComponent<GameDifficultyManagerScript>().SetDifficulty(GameDifficultyManagerScript.Difficulty.Normal);
+        Debug.Log("Setting Normal");
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Hard()
     {
+        StartCoroutine(Hard2());
+    }
+    IEnumerator Hard2()
+    {
         loadingScreen.SetActive(true);
-        gameDifficultyManagerScript.difficulty = GameDifficultyManagerScript.Difficulty.Hard;
+        GameObject.Find("GameDifficultyManager").GetComponent<GameDifficultyManagerScript>().SetDifficulty(GameDifficultyManagerScript.Difficulty.Hard);
+        Debug.Log("Setting Hard");
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
     public void Lunatic()
     {
+        StartCoroutine(Lunatic2());
+    }
+
+    IEnumerator Lunatic2()
+    {
         loadingScreen.SetActive(true);
-        gameDifficultyManagerScript.difficulty = GameDifficultyManagerScript.Difficulty.Lunatic;
+        GameObject.Find("GameDifficultyManager").GetComponent<GameDifficultyManagerScript>().SetDifficulty(GameDifficultyManagerScript.Difficulty.Lunatic);
+        Debug.Log("Setting Lunatic");
+        yield return new WaitForSeconds(1f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
     }
 
